003faef838063f36e80d1eb429f2cf368a1395fb
bakronz <164666400+bakronz@users.noreply.github.com>
2024-11-19 10:03:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menginisialisasi AOS untuk animasi scroll
+AOS.init({
+    duration: 2000, // Durasi animasi diperpanjang
+    easing: 'ease-in-out-quart', // Efek smooth scroll
+    once: true, // Animasi hanya terjadi sekali
+  });
+  
